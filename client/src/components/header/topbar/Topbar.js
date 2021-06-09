926c9b27593ae7ea4d6c003e84ea7b123c0a0ab5
ryu <ngothanhlongnoysiw@gmail.com>
2021-06-09 10:47:31 +0700

    pretty topbar time

--- client/src/components/header/topbar/Topbar.js
+++ client/src/components/header/topbar/Topbar.js
@@ -1,91 +1,43 @@
-import { useEffect, useState } from "react";
-import "./Topbar.css";
+import { useEffect, useState } from 'react';
+import './Topbar.css';
+
+const DAY = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+];
+
+const MONTH = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+
+const fomatDate = (date) => {
+    const month = date.getMonth();
+    const day = date.getDay();
+    const hours = `0${((date.getHours() + 11) % 12) + 1}`.slice(-2);
+    const minutes = `0${date.getMinutes()}`.slice(-2);
+    const seconds = `0${date.getSeconds()}`.slice(-2);
+    const midday = date.getHours() < 12 ? 'AM' : 'PM';
+    return `${MONTH[month]} ${DAY[day]} ${hours}:${minutes}:${seconds} ${midday}`;
+};
 
 const Topbar = () => {
-    const [date, setDate] = useState({});
-
-    const fomatDate = (date) => {
-        const dateObj = {};
-        dateObj.year = date.getFullYear();
-        dateObj.month = date.getMonth();
-        dateObj.date = date.getDate();
-        dateObj.day = date.getDay();
-        dateObj.hours = date.getHours();
-        dateObj.minutes = date.getMinutes();
-        dateObj.seconds = date.getSeconds();
-        return dateObj;
-    };
-
-    const fomatDay = (day) => {
-        let result = "";
-        switch (day) {
-            case 1:
-                result = "Monday";
-                break;
-            case 2:
-                result = "Tuesday";
-                break;
-            case 3:
-                result = "Wednesday";
-                break;
-            case 4:
-                result = "Thursday";
-                break;
-            case 5:
-                result = "Friday";
-                break;
-            case 6:
-                result = "Saturday";
-                break;
-            default:
-                result = "Sunday";
-                break;
-        }
-        return result;
-    };
-
-    const fomatMonth = (month) => {
-        let result = "";
-        switch (month) {
-            case 0:
-                result = "January";
-                break;
-            case 1:
-                result = "February";
-                break;
-            case 2:
-                result = "March";
-                break;
-            case 3:
-                result = "April";
-                break;
-            case 4:
-                result = "May";
-                break;
-            case 5:
-                result = "June";
-                break;
-            case 6:
-                result = "July";
-                break;
-            case 7:
-                result = "August";
-                break;
-            case 8:
-                result = "September";
-                break;
-            case 9:
-                result = "October";
-                break;
-            case 10:
-                result = "November";
-                break;
-            default:
-                result = "December";
-                break;
-        }
-        return result;
-    };
+    const [date, setDate] = useState('');
 
     useEffect(() => {
         setTimeout(() => {
@@ -103,19 +55,7 @@ const Topbar = () => {
                     <span>+84 039 406 669</span>
                 </i>
                 <i className="bi bi-clock d-flex align-items-center ms-3">
-                    <span>
-                        {fomatMonth(date.month)} {fomatDay(date.day)}{" "}
-                        {date.hours < 1
-                            ? `${date.hours + 12}`
-                            : date.hours < 10
-                            ? `0${date.hours}`
-                            : date.hours < 13
-                            ? `${date.hours}`
-                            : `0${date.hours - 12}`}
-                        :{date.minutes < 10 ? `0${date.minutes}` : date.minutes}
-                        :{date.seconds < 10 ? `0${date.seconds}` : date.seconds}{" "}
-                        {date.hours < 12 ? "AM" : "PM"}
-                    </span>
+                    <span>{date}</span>
                 </i>
             </div>
         </section>
